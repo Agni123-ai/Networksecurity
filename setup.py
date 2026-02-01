4b17df3c1a8ad602c628d8ca369a868cb34eec8d
Agni123-ai <ajoyroy251@gmail.com>
2026-02-01 22:14:58 +0530

    Setup.py file ready

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements() -> List[str]:
+    """This function will return list of requirements."""
+    requirement_list: List[str] = []
+    try:
+      with open("requirements.txt", "r") as f:
+        lines=f.readlines()
+        for line in lines:
+          requirement=line.strip()
+          ## ignore empty lines and -e.
+          if requirement and requirement!="-e .":
+            requirement_list.append(requirement)
+    except FileNotFoundError:
+      print("requirements.txt file not found.")
+    return requirement_list 
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Agnibho Roy Chowdhury",
+    author_email="ajoyroy251@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
