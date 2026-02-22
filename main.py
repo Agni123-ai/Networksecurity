499caccd83a15b68db1290eb080805e65fb1213d
Agni123-ai <ajoyroy251@gmail.com>
2026-02-22 14:41:29 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 if __name__=='__main__':
@@ -22,6 +23,13 @@ if __name__=='__main__':
     data_validation_artifact=data_validation.initate_data_validation()
     logging.info("data validation completed")
     print(data_validation_artifact)
+    
+    data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+    data_transformation=DataTransformation(data_validation_artifact=data_validation_artifact,data_transformation_config=data_transformation_config)
+    logging.info("initiating data transformation")
+    data_transformation_artifact=data_transformation.initiate_data_transformation()
+    logging.info("data transformation completed")
+    print(data_transformation_artifact)
     
     
   except Exception as e:
